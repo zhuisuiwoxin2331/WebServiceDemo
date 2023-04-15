c086ac3621596ea5026e6b311a8dd0f859e9e53b
zhuisuiwoxin2331 <97650574+zhuisuiwoxin2331@users.noreply.github.com>
2023-04-15 20:58:35 +0800

    Add files via upload

---
+++ MainTest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DBClass;
+using TestClass;
+
+namespace MainTest
+{
+    class Program
+    {
+        /// <summary>
+        /// 日志对象
+        /// </summary>
+        public static ServceLog log = new ServceLog();
+        /// <summary>
+        /// 
+        /// </summary>
+        public static SelectData Sd = new SelectData();
+        //public static DBCommon Db = new DBCommon();
+        //public static TestWriteXml Wr = new TestWriteXml();
+        //string xSQL = "", strSQL = ""; int iRet = -1;
+        //DataTable m_dt; static string strXml = "";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ServiceReferenceDMP.WebService2019SoapClient s = new ServiceReferenceDMP.WebService2019SoapClient();
+            
+            bool flag = false;//标记，只有值为true时，程序才继续运行
+            
+            try
+            {
+                new Mutex(false, System.Diagnostics.Process.GetCurrentProcess().ProcessName, out flag);
+                /*这里的标记名称，如果以Global\\开头，则在当前机器的所有登录用户会话中都保持唯一，
+                 * 如果以Local\\开头，则只在当前用户会话中保持唯一。
+                 * 不加则默认以Local\\开头
+                 */
+                new Mutex(false, "Global\\MainTest.exe", out flag);
+
+                if (!flag)//如果flag为false，表示已经有一个程序正在运行
+                {
+                    log.WriteInLog("程序已经在运行了，请不要重复运行！");
+                    Console.WriteLine("程序已经在运行了，请不要重复运行！");
+                    Thread.Sleep(3000);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.WriteInLog("MainTest() 发生异常，错误信息：" + ex.Message);
+            }
+
+            while (flag)//此处为死循环
+            {
+                try
+                {
+                    //Console.Clear();//清屏
+                    //Console.WriteLine(s.Print("a"));
+                    //先执行索取任务存储过程
+                    Console.WriteLine(Sd.ExeProData());
+                    //查询任务
+                    Sd.SelectTaskData();
+
+                    Console.WriteLine("本轮操作已经结束，主线程开始休眠，休眠时间（" + 1 + "）分钟");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Main() 执行时发生异常，错误信息：" + ex.Message);
+                  
+                }
+                Thread.Sleep(1 * 60 * 1000);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ MainTest/SelectData.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBClass;
+using TestClass;
+
+namespace MainTest
+{
+    public class SelectData
+    {
+        //声明需要调用的对象
+         DBCommon Db = new DBCommon();
+         TestWriteXml Wr = new TestWriteXml();
+         ServceLog log = new ServceLog();
+         ServiceReferenceDMP.WebService2019SoapClient s = new ServiceReferenceDMP.WebService2019SoapClient();
+        //声明变量
+         string xSQL = "", strSQL = ""; string strXml = ""; 
+         int iRet = -1, nRet = -1;
+         DataTable m_dt;  DataRow m_St;
+         bool nRflag = false;//标记是否返回成功
+        /// <summary>
+        /// //查询任务
+        /// </summary>
+        public void SelectTaskData()
+        {
+            try
+            {
+                Db.BeginTrans();
+                if (EfTaskData() > 0)//锁下发数据
+                {
+                    //log20190530 add FXL 新增分类型分别下发 
+
+
+                    //
+                    iRet = SelectTask(ref m_St);//查询下发数据
+                    if (m_dt.Rows.Count > 0 && iRet == 0)
+                    {
+                        //DataTable 转 XML 字符串
+                        strXml = Wr.DataTableToXml(m_dt);
+                        if (strXml.Substring(0, 1) == "N")
+                        {
+                            Db.RollbackTrans();
+                            log.WriteInLog("DataTable 转 XML 字符串失败！" + strXml);
+                            Console.WriteLine("DataTable 转 XML 字符串失败！"+ strXml);
+                            return;
+                        }
+                        //把XML 数据 下发到WCS下
+                        nRet=s.DMPProtocol(strXml);
+                        if (nRet == 1)//返回值若为1 说明 接口调用异常 需要重新发送
+                        {
+                            nRflag = true;
+                        }
+                        while (nRflag == true)//此处为死循环 直到发送成功为止
+                        {
+                            nRet = s.DMPProtocol(strXml);
+                            if (nRet !=1)//返回值若为1 说明 接口调用异常 需要重新发送
+                            {
+                                nRflag = false;
+                            }
+                        }
+                        //修改状态为发送（立库任务执行表）
+                        if (ElTaskData() > 0)
+                        {
+                            Db.CommitTrans();
+                            Console.WriteLine("任务下发成功！");
+                            log.WriteInLog("任务下发成功！");
+                            log.WriteInLog(strXml);
+                        }
+                        else 
+                        {
+                            Db.CommitTrans();
+                            Console.WriteLine("任务下发成功！但是修改状态出错，请检查！");
+                        }
+
+
+                    }
+                    else
+                    {
+                        Db.RollbackTrans();
+                        log.WriteInLog("目前没有任务下发......");
+                        Console.WriteLine("目前没有任务下发.....");
+                       
+                    }
+                }
+                else
+                {
+                    Db.RollbackTrans();
+                    log.WriteInLog("目前没有任务下发......");
+                    Console.WriteLine("目前没有任务下发.....");
+                     
+                }
+            }
+            catch (Exception ex)
+            {
+                Db.RollbackTrans();
+                log.WriteInLog("SelectTask(查询立库任务执行表)失败！" + ex.ToString());
+                Console.WriteLine("SelectTask(查询立库任务执行表)失败！" + ex.ToString());
+                
+            }
+        
+        }
+
+        /// <summary>
+        /// 查询SW_JobActionList(立库任务执行表)
+        /// </summary>
+        /// <returns></returns>
+        public int SelectTask(ref DataRow m_St)
+        {
+            try
+            {
+                strSQL = " select job_no,job_type,status,palette_no, pal_type,from_ware,from_address,to_ware,to_address,";
+                strSQL = strSQL + " priority,jobseq,export_time,finish_time,error_msg,create_time from sw_jobactionlist where status=-1 ";
+                m_dt = Db.dbSelect(strSQL).Tables[0];
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                log.WriteInLog("SelectTask(查询立库任务执行表)失败！" + ex.ToString());
+                Console.WriteLine("SelectTask(查询立库任务执行表)失败！" + ex.ToString());
+                return -1;
+            }
+        }
+        /// <summary>
+        /// 锁定任务数据（立库任务执行表）
+        /// </summary>
+        /// <returns></returns>
+        public int EfTaskData()
+        {
+            try
+            {
+                xSQL = " update SW_JobActionList set  status=-1  where status=0 ";
+                iRet = Db.dbExecute(xSQL);
+                return iRet;
+            }
+            catch (Exception ex)
+            {
+                log.WriteInLog("EfTaskData(锁定任务数据立库任务执行表)失败！" + ex.ToString());
+                return -1;
+            }
+
+        }
+        /// <summary>
+        /// 修改状态为发送（立库任务执行表）
+        /// </summary>
+        /// <returns></returns>
+        public int ElTaskData()
+        {
+            try
+            {
+                xSQL = " update SW_JobActionList set  status=1  where status=-1 ";
+                iRet = Db.dbExecute(xSQL);
+                return iRet;
+            }
+            catch (Exception ex)
+            {
+                log.WriteInLog("ElTaskData(修改状态为发送立库任务执行表)失败！" + ex.ToString());
+                return -1;
+            }
+
+        }
+        /// <summary>
+        /// 执行P_ASRS_NewTaskGet 索取新任务
+        /// </summary>
+        /// <returns></returns>
+        public string ExeProData()
+        {
+            try
+            {
+                OracleParameter op1 = new OracleParameter("AS_LOCNO", OracleType.VarChar);
+                op1.Value = "001";
+                OracleParameter op_msg = new OracleParameter("AS_MSG", OracleType.VarChar, 300);
+                op_msg.Direction = ParameterDirection.Output;
+
+                if (Db.ExecProc("P_ASRS_NEWTASKGET", new OracleParameter[] { 
+                               op1,op_msg }) <= 0)
+                {
+                    Db.RollbackTrans();
+                    Db.ConnClose();
+                    log.WriteInLog("P_ASRS_NEWTASKGET执行存储过程失败,请检查！");
+                    Console.WriteLine("P_ASRS_NEWTASKGET执行存储过程失败,请检查！");
+                    return "N";
+
+                }
+                if (op_msg.Value.ToString().Substring(0, 1) == "N")
+                {
+                    Db.RollbackTrans();
+                    Db.ConnClose();
+                    log.WriteInLog("P_ASRS_NEWTASKGET执行存储过程失败,请检查！" + op_msg.Value.ToString());
+                    Console.WriteLine("P_ASRS_NEWTASKGET执行存储过程失败,请检查！" + op_msg.Value.ToString());
+                    return op_msg.Value.ToString();
+                }
+                return op_msg.Value.ToString();
+            
+            }
+            catch (Exception ex)
+            {
+                Db.RollbackTrans();
+                Db.ConnClose();
+                log.WriteInLog("P_ASRS_NEWTASKGET执行存储过程失败,请检查！"+ex.ToString());
+                Console.WriteLine("P_ASRS_NEWTASKGET执行存储过程失败,请检查！" + ex.ToString());
+                return "N";
+            }
+
+            
+        }
+    }
+}
